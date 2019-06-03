32d58149e6c92ee1bf244e90fbd07874fb8946bf
Sofia Kwee <sofiakwee@gmail.com>
2019-06-03 17:37:07 +0000

    la

--- calculator.c
+++ calculator.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+   float multiply(float x)
+   {
+     float product = (x * x);
+     return product;
+   }
+
+   float divide(float x)
+   {
+     float quotient = (x / x);
+     return quotient;
+   }
+
+   float add(float x)
+   {
+     float sum = (x + x);
+     return sum;
+   }
+
+   float subtract(float x)
+   {
+     float difference = (x - x);
+     return difference;
+   }
