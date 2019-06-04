51b68fbd1506cd41ba6f702f55b043163167edd4
Sofia Kwee <sofiakwee@gmail.com>
2019-06-04 17:22:29 +0000

    commit

--- calculator.c
+++ calculator.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 
-   float multiply(float x)
+   float multiply(float value1, float value2)
    {
-     float product = (x * x);
+     float product = (value1 * value2);
      return product;
    }
 
-   float divide(float x)
+   float divide(float value1, float value2)
    {
-     float quotient = (x / x);
+     float quotient = (value1 / value2);
      return quotient;
    }
 
-   float add(float x)
+   float add(float value1, float value2)
    {
-     float sum = (x + x);
+     float sum = (value1 + value2);
      return sum;
    }
 
-   float subtract(float x)
+   float subtract(float value1, float value2)
    {
-     float difference = (x - x);
+     float difference = (value1 - value2);
      return difference;
    }
+
+   int main(int argc, char* argv[])
+   {
+     float value1;
+     int found = sscanf(argv[1], "%f", &value1);
+
+     float value2;
+     found = sscanf(argv[3], "%f", &value2);
+
+     char symbol;
+     found = sscanf(argv[2], "%c", &symbol);
+
+   if (&symbol == "x")
+    {
+     float result = multiply(value1, value2);
+     printf("Product of %f and %f = %f\n", value1, value2, result);
+    }
+}
